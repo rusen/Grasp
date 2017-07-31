e8eefc0d0668a61577c22961e46c563c47e34e1d
Umit Rusen Aktas <rusenaktas@gmail.com>
2017-07-31 17:02:13 +0100

    Fixing ubuntu CMake

---
+++ ubuntuBuild/CMakeFiles/basicGrasp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basicGrasp.dir/src/basicGrasp.o"
+  "CMakeFiles/basicGrasp.dir/src/controller/HandControllerInterface.o"
+  "CMakeFiles/basicGrasp.dir/src/controller/MPLHandController.o"
+  "CMakeFiles/basicGrasp.dir/src/planner/GraspPlanner.o"
+  "CMakeFiles/basicGrasp.dir/src/sensor/kinectSimulator.o"
+  "CMakeFiles/basicGrasp.dir/src/sensor/noiseutils.o"
+  "/home/xxa344/git/Grasp/mjpro150/bin/basicGrasp.pdb"
+  "/home/xxa344/git/Grasp/mjpro150/bin/basicGrasp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basicGrasp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
