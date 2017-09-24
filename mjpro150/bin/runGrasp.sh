6fedc6efcdc4795234596a4f84ef9ef85cf7213c
Umit Rusen Aktas <rusenaktas@gmail.com>
2017-09-24 20:14:28 +0100

    Adding run grasp code.

--- mjpro150/bin/runGrasp.sh
+++ mjpro150/bin/runGrasp.sh
@@ -1,36 +1,10 @@
 #!/bin/bash
 
-task(){
-   PORT=$(((RANDOM<<15)|RANDOM))
-   ./basicGrasp ../model/BHAM /home/rusi/Dropbox visualOff $PORT
-}
-
-open_sem(){
-    mkfifo pipe-$$
-    exec 3<>pipe-$$
-    rm pipe-$$
-    local i=$1
-    for((;i>0;i--)); do
-        printf %s 000 >&3
-    done
-}
-run_with_lock(){
-    local x
-    read -u 3 -n 3 x && ((0==x)) || exit $x
-    (
-    "$@" 
-    printf '%.3d' $? >&3
-    )&
-}
-
-# Delete temp files                                                                                                                                                                                     
 rm -rf ./tmp
 rm -rf ../model/BHAM/*_include*.xml
 rm -rf ../model/BHAM/*_Test.xml
 
-N=5
-open_sem $N
-while true; do
-    run_with_lock task
-done 
-wait
+N=4
+DROPBOXFOLDER=/home/rusi/Dropbox
+
+parallel --timeout 900 --jobs $N ./basicGrasp ../model/BHAM $DROPBOXFOLDER visualOff $RANDOM ::: {1..10000}
