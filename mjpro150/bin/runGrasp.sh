951f9162d4ce890fb623a3b2dbbde56148e42343
rusen <rusenaktas@gmail.com>
2017-09-25 10:38:32 +0100

    Adding arguments to runGrasp.sh

--- mjpro150/bin/runGrasp.sh
+++ mjpro150/bin/runGrasp.sh
@@ -4,7 +4,4 @@ rm -rf ./tmp
 rm -rf ../model/BHAM/*_include*.xml
 rm -rf ../model/BHAM/*_Test.xml
 
-N=4
-DROPBOXFOLDER=/home/rusi/Dropbox
-
-parallel --timeout 900 --jobs $N ./basicGrasp ../model/BHAM $DROPBOXFOLDER visualOff $RANDOM ::: {1..10000}
+parallel --timeout 900 --jobs $1 ./basicGrasp ../model/BHAM $2 visualOff $RANDOM ::: {1..10000}
