766d51180e58d14094f97346d554dda316e34042
Rusen Aktas <rusenaktas@gmail.com>
2018-06-27 15:54:40 +0000

    More analysis code

--- mjpro150/bin/AnalyzeGrasps.py
+++ mjpro150/bin/AnalyzeGrasps.py
@@ -9,6 +9,7 @@ classCount = 24;
 def count_grasps(setText, fileName, orderFile = None):
    
    d = './allData'
+   dOrg = './allDataOrg'
    scenes = 0
    classSuccess = []
    classTotal = []
@@ -33,6 +34,14 @@ def count_grasps(setText, fileName, orderFile = None):
    topSuccess = 0
    returnArr = []
    folderList = filter(os.path.isdir, [os.path.join(d,f) for f in os.listdir(d)])
+
+   changes = [[0, 0, 0], [0, 0, 0], [0,0,0]]
+
+   # Keep track of changes.
+   maxScene = 0
+   maxSceneCount = -1
+   maxScenePath = []
+
    for item in folderList:
       setName = os.path.basename(os.path.normpath(item))
       folderList2 = filter(os.path.isdir, [os.path.join(item,f) for f in os.listdir(item)])
@@ -50,10 +59,20 @@ def count_grasps(setText, fileName, orderFile = None):
           for itemScene in folderListScenes:
               filePath = itemScene + "/" +  fileName
               scenes = scenes + 1
+              sceneId = os.path.basename(os.path.normpath(itemScene))
+
+              # Read grasp data.
               floatSize = os.path.getsize(filePath)/4
               graspData = array.array('f')
               f = open(filePath, 'rb')
               graspData.fromfile(f, floatSize)
+              f.close()
+
+              # Read original file here.
+              orgGraspData = array.array('f')
+              orgFilePath = dOrg + '/' + setName + '/' + str(objectId+1) + '/' + sceneId + "/data.bin"
+              f = open(orgFilePath, 'rb')
+              orgGraspData.fromfile(f, floatSize)
               f.close()
 
               # Get desired order
@@ -67,20 +86,26 @@ def count_grasps(setText, fileName, orderFile = None):
               if (floatSize/285) < 10:
                   continue;
               for itr in range(0, (floatSize/285)):
-                  total = total + 1
+                 total = total + 1
+                 
+                 # Record-keeping about the grasps in general.
+     #            print(str(int(-orgGraspData[itr * 285] + 1)) + ':' + str(int(-graspData[itr * 285] + 1)))
+                 changes[int(-orgGraspData[itr * 285] + 1)][int(-graspData[itr * 285] + 1)] = \
+                     changes[int(-orgGraspData[itr * 285] + 1)][int(-graspData[itr * 285] + 1)] + 1
+
                  # if graspData[itr * 285] > -0.5:
-                  graspTotal[itr] = graspTotal[itr] + 1
-                  classTotal[classId] = classTotal[classId] + 1
-                  if itr==topRank:
+                 graspTotal[itr] = graspTotal[itr] + 1
+                 classTotal[classId] = classTotal[classId] + 1
+                 if itr==topRank:
                      firstClassTotal[classId] = firstClassTotal[classId] + 1
-                  if graspData[itr * 285] >= 0.5:
+                 if graspData[itr * 285] >= 0.5:
                       successful = successful + 1
                       graspSuccess[itr] = graspSuccess[itr] + 1
                       classSuccess[classId] = classSuccess[classId] + 1
                       if itr==topRank:
                          topSuccess = topSuccess + 1
                          firstClassSuccess[classId] = firstClassSuccess[classId] + 1
-                  elif graspData[itr*285] < -0.5:
+                 elif graspData[itr*285] < -0.5:
                       collide = collide + 1
    returnArr.append(scenes)
    returnArr.append(successful)
@@ -115,7 +140,8 @@ def count_grasps(setText, fileName, orderFile = None):
    print "TOP SUCCESS:" + str(float(topSuccess)/totalFirstGrasps)
    print topSuccess
    print totalFirstGrasps
- 
+   print(changes) 
+
    return returnArr
  
 classFile = open("classIds.txt")
